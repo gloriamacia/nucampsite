d8bfb6fc6fd35e2be5c8a17ecdffe3bc4c431ff7
Gloria Macia <gloria.macia@roche.com>
2024-07-16 23:52:14 +0200

    add weather exercise week 5

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,54 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 })   
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    console.log(apiKey)
+    const city = 'Barcelona';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        displayWeather(data);
+    } catch (error) {
+        console.error('Error fetching the weather data:', error);
+    }
+}
+
+function displayWeather(data){
+    console.log(data.main.temp);
+    console.log(data.weather[0].description);
+    console.log(data.weather[0].icon);
+
+    // select DOM elements
+    const weatherIcon = document.getElementById("weather-icon");
+    const weatherTemp = document.getElementById("weather-temp");
+    const weatherDescription = document.getElementById("weather-description");
+    const weatherCity = document.getElementById("weather-city");
+
+    const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const iconImg = document.createElement("img");
+    iconImg.src = iconUrl;
+    iconImg.alt = "Weather Icon";
+    weatherIcon.appendChild(iconImg); 
+
+    // temperature (including degree symbol)
+    weatherTemp.textContent = `${data.main.temp} \u00B0C`;
+
+    // Set weather description text content
+    weatherDescription.textContent = data.weather[0].description;
+
+    weatherCity.textContent = `in ${data.name}`;
+
+    // Ensure weather container is displayed (remove 'd-none' class)
+    const weatherContainer = document.getElementById("weather");
+    weatherContainer.classList.remove("d-none");
+}
+
+fetchWeather();
